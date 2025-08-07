2e196ccf3047330875753238a24f76bede2d7532
Luan Gamers <gamerscaua@gmail.com>
2025-08-07 14:51:38 -0300

    com footer

--- src/paginas/Sobre.js
+++ src/paginas/Sobre.js
@@ -1,5 +1,6 @@
 import Banner from '../componentes/Banner';
 import NavBar from '../componentes/NavBar';
+import Footer from '../componentes/Footer';
 function Sobre() {
   return (
     <div className="pagina">
@@ -9,7 +10,9 @@ function Sobre() {
     <p>A IA está em constante evolução. Espera-se que ela assuma papéis ainda mais complexos, colaborando com humanos em decisões importantes.</p>
     <p>Desafios como ética, privacidade e impacto no emprego também estão em discussão, exigindo atenção da sociedade.</p>
     <p>Com responsabilidade e inovação, o futuro da IA pode ser promissor e transformador.</p>
+    <Footer/>
   </div>
+
   );
 }
 
